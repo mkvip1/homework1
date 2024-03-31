a932f5c514d5198955951c820228c38a99d03045
ms.kuznetcov <ms.kuznetcov@stalcom.com>
2024-03-31 14:29:55 +0300

    add AsyncConfig.java

--- src/main/java/com/example/tracktime/TrackTimeApplication.java
+++ src/main/java/com/example/tracktime/TrackTimeApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import com.example.tracktime.service.TestService;
 
-@EnableAsync
 @SpringBootApplication
 public class TrackTimeApplication {
 
